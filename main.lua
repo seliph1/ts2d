7569c8b01fb44e9f4548b153e0a2416beb917cf6
seliph1 <gabriel_costa_mendes@hotmail.com>
2026-06-21 13:55:41 -0300

    testing with fps controller unit

--- main.lua
+++ main.lua
@@ -40,7 +40,7 @@ if love.getVersion() < 12 then
 	love.graphics.newTextBatch = love.graphics.newText
 end
 
---[[
+
 local fps_cap = 1/120
 --fps_cap = 0.001
 
@@ -82,7 +82,7 @@ function love.run()
 
 		if love.timer then love.timer.sleep(fps_cap) end
 	end
-end]]
+end
 
 local loveframes 	= require "lib.loveframes"
 local console 		= require "core.interface.console"
